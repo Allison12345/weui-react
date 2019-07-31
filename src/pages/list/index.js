3379c34ff46e0f1460e525e0dec13db0061ebfbd
Allison12345 <2562392373@qq.com>
2019-07-31 16:17:12 +0800

    list

--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -13,6 +13,45 @@ const List = () => {
         <WeuiCell bd="标题文字" ft="说明文字" />
         <WeuiSwiped />
       </WeuiCells>
+      <WeuiCells title="带图标、说明的列表项" />
+      <WeuiCells>
+        <WeuiCell
+          type="example"
+          hd={() => (
+            <img
+              src={require('../../assets/imgs/weui-littlePanel.png')}
+              alt=""
+              style={{ width: '20px', marginRight: '16px', display: 'block' }}
+            />
+          )}
+          bd="标题文字"
+          ft="说明文字"
+        />
+      </WeuiCells>
+      <WeuiCells title="带跳转的列表项" />
+      <WeuiCells>
+        <WeuiCell type="access" bd="cell standard" ft />
+      </WeuiCells>
+      <WeuiCells title="带说明、跳转的列表项" />
+      <WeuiCells>
+        <WeuiCell type="access" bd="cell standard" ft="说明文字" />
+      </WeuiCells>
+      <WeuiCells title="带图标、说明、跳转的列表项" />
+      <WeuiCells>
+        <WeuiCell
+          type="access"
+          example
+          hd={() => (
+            <img
+              src={require('../../assets/imgs/weui-littlePanel.png')}
+              alt=""
+              style={{ width: '20px', marginRight: '16px', display: 'block' }}
+            />
+          )}
+          bd="标题文字"
+          ft="说明文字"
+        />
+      </WeuiCells>
     </Page>
   )
 }
