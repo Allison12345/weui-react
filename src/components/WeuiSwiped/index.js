660a1d99c188dd025fc0f03997277d0d75a63cea
Allison12345 <2562392373@qq.com>
2019-07-31 15:49:06 +0800

    滑动删除

--- src/components/WeuiSwiped/index.js
+++ src/components/WeuiSwiped/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 import WeuiCell from '../../components/WeuiCell'
 
@@ -6,32 +6,65 @@ class WeuiSwiped extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      index: 0
+      index: 0,
+      isshown: true
     }
   }
   touchstart = e => {
     this.touchStart = e.touches[0].clientX
   }
-  touchend = () => {}
+  touchend = () => {
+    if (this.dist > 0) {
+      if (this.dist < 34) {
+        this.setState({ index: `${-68}px` })
+      } else if (34 <= this.dist && this.dist <= 68) {
+        this.setState({ index: 0 })
+      }
+    } else if (this.dist < 0) {
+      if (this.dist > -34) {
+        this.setState({ index: 0 })
+      } else if (-68 <= this.dist && this.dist <= -34) {
+        this.setState({ index: `${-68}px` })
+      }
+    }
+  }
   touchmove = e => {
-    const dist = e.changedTouches[0].clientX - this.touchStart
-    console.log(dist)
-    this.setState({ index: `${dist}px` })
+    this.dist = e.changedTouches[0].clientX - this.touchStart
+    if (this.dist < 0) {
+      this.setState({ index: `${this.dist}px` })
+      if (this.dist < -68) return
+    } else if (this.dist > 0) {
+      this.setState({ index: `${-68 + this.dist}px` })
+      if (this.dist > 68) return
+    }
+
+    console.log(this.dist)
   }
   render() {
     return (
-      <div
-        className="weui-cell weui-cell_swiped"
-        style={{ transform: `translateX(${this.state.index})` }}
-        onTouchStart={this.touchstart}
-        onTouchEnd={this.touchend}
-        onTouchMove={this.touchmove}
-      >
-        <WeuiCell bd="标题文字" ft="说明文字" />
-        <div className="weui-cell__ft">
-          <div className="weui-swiped-btn weui-swiped-btn_warn">删除</div>
-        </div>
-      </div>
+      <Fragment>
+        {this.state.isshown && (
+          <div
+            className="weui-cell weui-cell_swiped"
+            onTouchStart={this.touchstart}
+            onTouchEnd={this.touchend}
+            onTouchMove={this.touchmove}
+          >
+            <div
+              className="weui-cell__bd"
+              style={{ transform: `translateX(${this.state.index})` }}
+            >
+              <WeuiCell bd="标题文字" ft="说明文字" />
+            </div>
+            <div
+              className="weui-cell__ft"
+              onClick={() => this.setState({ isshown: false })}
+            >
+              <div className="weui-swiped-btn weui-swiped-btn_warn">删除</div>
+            </div>
+          </div>
+        )}
+      </Fragment>
     )
   }
 }
